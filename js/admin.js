8cefc6452cead5444b264e18fdc0748d4e6a5b21
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-07-24 19:44:46 -0300

    Agregue el modal para agregar canciones y el archivo admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,14 @@
-// Script del admin
+import { Cancion } from "./cancionClass.js";
+
+let nuevaCancion = new Cancion();
+console.log(nuevaCancion);
+
+let codigo = document.getElementById('codigo');
+let autor = document.getElementById('autor');
+let titulo = document.getElementById('titulo');
+let album = document.getElementById('album');
+let duracion = document.getElementById('duracion');
+let genero = document.getElementById('genero');
+let formulario = document.getElementById('formCancion');
+
+
