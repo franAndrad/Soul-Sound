34370e1872fb3a4442d3c4e16e77eabd0237a1f6
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-07-24 21:50:10 -0300

    Agregue las funciones para el create

--- js/admin.js
+++ js/admin.js
@@ -9,6 +9,71 @@ let titulo = document.getElementById('titulo');
 let album = document.getElementById('album');
 let duracion = document.getElementById('duracion');
 let genero = document.getElementById('genero');
-let formulario = document.getElementById('formCancion');
+let formulario = document.querySelector('#formCancion');
+let btnCrearCancion = document.querySelector('#btnCrearCancion');
+const modalAdminCancion = new bootstrap.Modal(document.getElementById('modalCancion'));
+console.log(modalAdminCancion);
+
+let cancionExistente = false;
+
+let listaCanciones = JSON.parse(localStorage.getItem('listaCancionesKey')) || [];
+
+//validaciones
+
+formulario.addEventListener('submit', guardarCancion);
+
+function guardarCancion(e){
+    e.preventDefault();
+    if(cancionExistente){
+
+        guardarEdicionCancion();
+    } else {
+        crearCancion();
+    }
+
+}
 
 
+function crearCancion(e){
+    e.preventDefault();
+    console.log('desde crear cancion');
+    let nuevaCancion = new Cancion(codigo.value, autor.value, titulo.value, album.value, duracion.value, genero.value);
+    console.log(nuevaCancion);
+    listaCanciones.push(nuevaCancion);
+    limpiarFormulario();
+    guardarListaCanciones();
+    modalAdminCancion.hide();
+
+    crearFila(nuevaCancion);
+}
+
+function limpiarFormulario(){
+    formulario.reset();
+}
+
+function guardarListaCanciones(){
+    localStorage.setItem('listaCancionesKey', JSON.stringify(listaCanciones));
+}
+
+function cargaInicial(){
+    if(listaCanciones.length > 0){
+        listaCanciones.forEach((itemCancion)=>{crearFila(itemCancion)})
+    }
+}
+
+function crearFila(itemCancion){
+    let tablaCanciones = document.getElementById('listaCanciones');
+    tablaCanciones.innerHTML += `
+    <tr>
+        <th scope="row">${itemCancion.codigo}</th>
+        <td>${itemCancion.autor}</td>
+        <td>${itemCancion.titulo}</td>
+        <td>${itemCancion.album}</td>
+        <td>${itemCancion.duracion}</td>
+        <td>${itemCancion.genero}</td>
+        <td>
+            <button class="btn btn-light m-1 opacity-75"><i class="bi bi-pencil-square"></i></button>
+            <button class="btn btn-danger m-1 opacity-75"><i class="bi bi-x-square-fill"></i></button>
+        </td>
+    </tr>`
+}
