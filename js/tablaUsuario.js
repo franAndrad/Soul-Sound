fab125464a9e9db29f658c155f0c3a7fbf76cb5d
Juan Menichetti <jpmenichetti13@gmail.com>
2022-07-25 17:07:42 -0300

    agregamos la pregunta para borrar con el sweetalert

--- js/tablaUsuario.js
+++ js/tablaUsuario.js
@@ -1,7 +1,8 @@
 // funciones para crear la lista de usuarios en administrador
+import { Usuario } from "./loginRegistro.js";
 import {listaUsuario} from "./nav.js";
 
-
+// funciona para crear la tabla de usuarios
 cargaInicialUsuario();
 
 function cargaInicialUsuario(){
@@ -21,4 +22,32 @@ function crearFilaUsuario(itemUsuario){
             <button class="btn btn-danger m-1 opacity-75"><i class="bi bi-x-square-fill"></i></button>
         </td>
     </tr>`
+}
+
+// funcion para borrar los usuarios
+
+window.borrarUsuario = function (codigo){
+    console.log(codigo);
+    // pregunto si quiero borrar al usuario
+    Swal.fire({
+            title: "Estas seguro de querer eliminar al usuario?",
+            text: "No puedes revertir este paso luego de aceptar.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, borrar.',
+            cancelButtonText: 'Cancelar.'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                // borrar el usuario de listaUsario y tambien del localStorage
+                // actualizar la tabla
+                // mostrar el cartel de operacion exitosa
+              Swal.fire(
+                'Usuario eliminado!',
+                'El usuario fue eliminado exitosamente.',
+                'success'
+              )
+            }
+          })
 }
